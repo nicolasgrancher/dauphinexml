bee0a4096797c338e0e03704b589276cc601cb2e
Nicolas.Gaye <Nicolas.Gaye@cdebf6e4-5e5e-11de-bbaa-f1a44a2b9d47>
2009-06-27 00:29:05 +0000

    nomsg

---
+++ cv/connexion.php
@@ -0,0 +1,15 @@
+<form method="post" action="">
+	<table>
+		<tr>
+			<th><label for="login">Login : </label></th>
+			<td><input type="text" id="login" name="login" /></td>
+		</tr>
+		<tr>
+			<th><label for="mdp">Mot de pase : </label></th>
+			<td><input type="text" id="mdp" name="mdp" /></td>
+		</tr>
+		<tr>
+			<td colspan="2"><input type="submit" value="S'inscrire" /></td>
+		</tr>
+	</table>
+</form>
